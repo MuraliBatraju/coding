27703441994ec1d4cf1f28b256126e6fcbcaf650
MuraliBatraju <muralibatraju66@gmail.com>
2024-05-25 13:25:52 +0530

    Add files via upload

---
+++ LAB2/UserLogin.java
@@ -0,0 +1,15 @@
+public class UserLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String userlogin ="murali";
+		if( userlogin=="murali") {
+			System.out.println("user login successfully");
+		}
+		else {
+			System.out.println("user login UnSuccessfully! error");
+		}
+
+	}
+
+}
