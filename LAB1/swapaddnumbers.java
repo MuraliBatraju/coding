ef342802d0706cbe8f20bd29c3e433615a485f36
MuraliBatraju <muralibatraju66@gmail.com>
2024-05-25 13:21:53 +0530

    Add files via upload

---
+++ LAB1/swapaddnumbers.java
@@ -0,0 +1,18 @@
+public class swapaddnumbers {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	     int a=6;
+	     int b=8,sum;
+	    		 System.out.println("Before swapping:"+"a="+a+",b="+b);
+	     a=a+b;
+	     b=a-b;
+	     a=a-b;
+	     System.out.println("After swapping:"+"a="+a+",b="+b);
+	     sum=a+b;
+	     System.out.println("The sum of two numbers is:"+sum);
+
+
+	}
+
+}
